208ce5046364900d691afe8c766d4fbd5620e9d5
Ajit Mote <123333490+AjitMote42@users.noreply.github.com>
2024-01-02 22:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".navformdDevice").style.display = "block"
+})
+document.querySelector(".closebtn").addEventListener("click", () => {
+    document.querySelector(".navformdDevice").style.display = "none"
+})
+
+console.log("ssd");
