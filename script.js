2605c85892fa34b56c3b0315d9282a1597a3ae44
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2024-01-08 22:05:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
-
 document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".navformdDevice").style.display = "block"
+    document.querySelector(".navformdDevice").style.right = "0"
 })
 document.querySelector(".closebtn").addEventListener("click", () => {
-    document.querySelector(".navformdDevice").style.display = "none"
+    document.querySelector(".navformdDevice").style.right = "-100%"
 })
 
 document.querySelectorAll(".qutetoggle").forEach(e => {
@@ -30,29 +29,23 @@ document.querySelector(".qute img").addEventListener("click", () => {
 //add event listner for to form validation
 let emaill = document.getElementById("Email1")
 let text = document.getElementById("floatingTextarea")
-
-// document.querySelector("#submit").addEventListener("click", ()=>{
-// if (emaill.value ==="" && text.value ==="") {
-//     e-valid.innerHTML== "is empty"
-// }
-// })
-
-
 let mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()
 
+};
+//function
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    
   } else {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
